010ec0dc5c5c63ca4ba96e480182e48d71d2afeb
Aakash Verma <aakashverma@192.168.1.4>
2021-08-16 10:20:59 +0530

    create user added in handlers.go

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -2,8 +2,11 @@ package middleware
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"go-crud/models"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -41,4 +44,34 @@ func createConnection() *sql.DB {
 	return db
 }
 
+// CreateUser create a user in the postgres db
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	// set the header to content type x-www-form-urlencoded
+	// Allow all origin to handle cors issue
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
+	// create an empty user of type models.User
+	var user models.User
+
+	// decode the json request to user
+	err := json.NewDecoder(r.Body).Decode(&user)
+
+	if err != nil {
+		log.Fatalf("Unable to decode the request body.  %v", err)
+	}
+
+	// call insert user function and pass the user
+	insertID := insertUser(user)
+
+	// format a response object
+	res := response{
+		ID:      insertID,
+		Message: "User created successfully",
+	}
+
+	// send the response
+	json.NewEncoder(w).Encode(res)
+}
